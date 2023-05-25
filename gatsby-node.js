9d8f18c811c57b4a37e114d1dc06f1b9e411ac41
muhammad8584ft <91459241+muhammad8584ft@users.noreply.github.com>
2023-05-25 14:17:46 +0700

    Create gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,42 @@
+const { createFilePath } = require("gatsby-source-filesystem")
+const path = require("path")
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode })
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/post.js"), // Ganti dengan path ke template post Anda
+      context: {
+        // Data yang akan disediakan ke template post Anda
+        slug: node.fields.slug,
+      },
+    })
+  })
+}
