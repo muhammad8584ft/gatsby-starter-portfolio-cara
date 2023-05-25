fb9bbcd31f3bc70cba7303b8689045f89961480f
muhammad8584ft <91459241+muhammad8584ft@users.noreply.github.com>
2023-05-25 15:23:47 +0700

    Update icao.tsx

--- src/pages/icao.tsx
+++ src/pages/icao.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { HeadFC, Link, PageProps } from "gatsby"
+import { Link } from "gatsby"
 import { Parallax } from "@react-spring/parallax"
 import { Themed } from "@theme-ui/mdx"
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
@@ -10,37 +10,34 @@ import Seo from "@lekoarts/gatsby-theme-cara/src/components/seo"
 import Content from "@lekoarts/gatsby-theme-cara/src/elements/content"
 import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
 
-const NotFound = (_props: PageProps) => (
+const PDFViewer = () => {
+  React.useEffect(() => {
+    const loadPDF = (fileId) => {
+      const pdfViewer = new window.google.drive.viewer.PdfViewer(
+        document.getElementById("pdfViewer")
+      )
+      pdfViewer.setViewerUrl(
+        `https://drive.google.com/file/d/${fileId}/preview`
+      )
+      pdfViewer.setPdfUrl(
+        `https://drive.google.com/uc?export=download&id=${fileId}`
+      )
+      pdfViewer.open()
+    }
+
+    // Panggil fungsi loadPDF dengan ID file PDF yang ingin ditampilkan
+    loadPDF("1kc563mPIrjIYBtOvbGU5M-Nv_25b7P_F")
+  }, [])
+
+  return <div id="pdfViewer"></div>
+}
+
+const NotFound = () => (
   <Layout>
     <Parallax pages={1}>
       <div>
         <Divider speed={0.2} offset={0} factor={1}>
-          <UpDown>
-            <Svg icon="triangle" hiddenMobile width={48} stroke color="icon_orange" left="10%" top="20%" />
-            <Svg icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%" />
-            <Svg icon="box" width={6} color="icon_darker" left="60%" top="15%" />
-          </UpDown>
-          <UpDownWide>
-            <Svg icon="arrowUp" hiddenMobile width={16} color="icon_blue" left="80%" top="10%" />
-            <Svg icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
-            <Svg icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
-            <Svg icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
-            <Svg icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
-            <Svg icon="circle" width={6} color="icon_darkest" left="75%" top="10%" />
-            <Svg icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
-          </UpDownWide>
-          <Svg icon="circle" hiddenMobile width={24} color="icon_darker" left="5%" top="70%" />
-          <Svg icon="circle" width={6} color="icon_darkest" left="4%" top="20%" />
-          <Svg icon="circle" width={12} color="icon_darkest" left="50%" top="60%" />
-          <Svg icon="upDown" width={8} color="icon_darkest" left="95%" top="90%" />
-          <Svg icon="upDown" hiddenMobile width={24} color="icon_darker" left="40%" top="80%" />
-          <Svg icon="triangle" width={8} stroke color="icon_darker" left="25%" top="5%" />
-          <Svg icon="circle" width={64} color="icon_green" left="95%" top="5%" />
-          <Svg icon="box" hiddenMobile width={64} color="icon_purple" left="5%" top="90%" />
-          <Svg icon="box" width={6} color="icon_darkest" left="10%" top="10%" />
-          <Svg icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
-          <Svg icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
-          <Svg icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
+          {/* ... */}
         </Divider>
         <Content sx={{ variant: `texts.bigger` }} speed={0.4} offset={0} factor={1}>
           <Inner>
@@ -48,6 +45,7 @@ const NotFound = (_props: PageProps) => (
             <Themed.p>
               Go back to <Link to="/">homepage</Link>.
             </Themed.p>
+            <PDFViewer />
           </Inner>
         </Content>
       </div>
@@ -56,5 +54,3 @@ const NotFound = (_props: PageProps) => (
 )
 
 export default NotFound
-
-export const Head: HeadFC = () => <Seo title="404 - Not Found" />
